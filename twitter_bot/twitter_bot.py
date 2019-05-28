71c0fea140626714adfc45d9ef59aed42bf09d66
0140831 <matsu.hide009@gmail.com>
2019-05-29 00:53:17 +0900

    cahnge []

--- twitter_bot/twitter_bot.py
+++ twitter_bot/twitter_bot.py
@@ -1,8 +1,14 @@
+#!/usr/local/bin python3.7
+# -*- coding: utf-8 -*-
+
+# exe python3.7 twitter_bot.py
+
 import os
 import json
 import logging
 
 import twitter
+import show_CO2_TEMP
 
 
 class Twitter_bot:
@@ -14,15 +20,16 @@ class Twitter_bot:
 
     '''
     # logfile
-    logging.basicConfig(level=logging.DEBUG, filename='.\\Log.txt',
+    logging.basicConfig(level=logging.DEBUG, filename='./Log.txt',
                         format=' %(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
 
     # twitter tokenを記述しておく
-    _jsondata = ".\\twitter_token.json"
+    _jsondata = "./twitter_token.json"
 
     def load_JSON(self):
         '''JSONの読み取り class定数の”_jsondata”を読み取る
         '''
+        logging.debug('VV')
 
         # Json読み取り
         return json.load(open(self._jsondata, 'r'))
@@ -30,33 +37,44 @@ class Twitter_bot:
     def set_token(self, obj_json):
         '''読み込んだjsonからtwitter tokenの取り出しtwitterObjectをリターンする．
         '''
+        logging.debug('VV')
 
         return twitter.Twitter(auth=twitter.OAuth(
             obj_json['token'], obj_json['token_secret'], obj_json['consumer_key'], obj_json['consumer_secret']))
 
-    def update_tweet(self, obj_twitter):
-        '''ツイートする'''
+    def get_SensorInf(self):
+        logging.debug('VV')
+        sensor = show_CO2_TEMP.show_CO2_TEMP
+        return sensor.get_Inf()
 
-        # 投稿メッセージ
-        msg = "TEST_MSG"
+    def update_tweet(self, obj_twitter, msg):
+        '''ツイートする'''
+        logging.debug('VV')
 
         # Twitterに投稿
         rst = obj_twitter.statuses.update(status=msg)
-        logging.debug(rst)
+        # logging.debug(rst)
+
+        logging.debug('AA')
 
     def main(self):
         '''メイン'''
+        logging.debug('VV')
 
         obj_json = self.load_JSON(self)
         obj_twitter = self.set_token(self, obj_json)
-        self.update_tweet(self, obj_twitter)
+        msg = self.get_SensorInf(self)
+        logging.debug(msg)
+        self.update_tweet(self, obj_twitter, msg)
+
+        logging.debug('AA')
 
     def __init__(self):
         ''''''
 
 
 if __name__ == "__main__":
-    # ダブルクリックなどで実行された場合に”__name__”に”__name__”と入るのでここが実行される。
+        # ダブルクリックなどで実行された場合に”__name__”に”__name__”と入るのでここが実行される
     logging.debug('VV')
     myclass = Twitter_bot
     myclass.main(myclass)
