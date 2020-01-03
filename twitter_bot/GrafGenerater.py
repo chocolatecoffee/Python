5dd556c5cde5537dd6fc514915f0943e8b3b5e33
0140831 <matsu.hide009@gmail.com>
2020-01-04 00:41:53 +0900

    基礎は完成．　「LoadAndAddJSON」の作成するデータ形式に合わせロジックの変更

--- twitter_bot/GrafGenerater.py
+++ twitter_bot/GrafGenerater.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
@@ -20,7 +21,9 @@ logging.basicConfig(level=logging.DEBUG, filename='./Log.txt', filemode='w',form
 # _today = datetime.datetime.now().isoformat()
 # _today 2019-10-17T23:11:56.338690
 
+
 _today = datetime.datetime.now()
+#_today = datetime.date(2020,1,1)
 '''datetime obj "Today" '''
 
 _yesterday = _today - timedelta(days=1)
@@ -48,43 +51,48 @@ class GrafGenerater:
     '''昨日のセンサーデータの入ったjsonファイル名を指定 "2019_SensorData.json"と先頭に年が入る
     '''
 
-    def isYesterday(self):
-        '''昨日は去年か，先月か，同じ月の機能かの判定パターン（yesterdayPattern）を入れる．\n
-            'yesterday'・・・同じ月の昨日\n
-            'lastMonth'・・・先月\n
-            'lastYear'・・・去年\n
+    def _isYesterday(self):
+        '''
+            昨日は去年か，先月か，同じ月の機能かの判定パターンを入れる．\n
         
         Returns:
-            [type]: [String]
+            str: 'yesterday'・・・同じ月の昨日,'lastMonth'・・・先月,'lastYear'・・・去年
         '''
-
         logging.debug('VV')
-
-        yesterdayPattern = ''
+        
+        # 昨日は年末か月末か昨日かの文字列を入れる
+        _ = ''
 
         # 今日の日付と昨日の日付を比較
         # 年は同じ，月は同じ，今日の日付が大きい
         if (_today.year == _yesterday.year and _today.month == _yesterday.month and _today.day > _yesterday.day):
-            yesterdayPattern = 'yesterday'
+            _ = 'yesterday'
         # 年は同じ，昨日
         elif (_today.year == _yesterday.year and _today.month > _yesterday.month):
-            yesterdayPattern = 'lastMonth'
+            _ = 'lastMonth'
         # 昨年
         elif (_today.year > _yesterday.year):
-            yesterdayPattern = 'lastYear'
-
+            _ = 'lastYear'
+        
+        #_ = 'yesterday'
+        #_ = 'lastMonth'
+        #_ = 'lastYear'
+        
         logging.debug('AA')
 
-        return yesterdayPattern
+        return _
 
     def _load_JSON(self, jsonFile):
         '''_jsonfileをよみだす
         
         Args:
-            _jsonfile ([type]): [description]
+            jsonFile ([str]): [_jsonfile・・・センサーデータが記載されたファイル名]
+        
+        Returns:
+            [jsonobj]: [_jsonfileを読みだしたもの]
         '''        
-        logging.debug('VV')
 
+        logging.debug('VV')
         return json.load(open(jsonFile, 'r'))
 
     def repackSensordata(self):
@@ -107,15 +115,18 @@ class GrafGenerater:
 
         #logging.debug(type(jsonobj))
 
-        # 一日ごとのセンサーデータ保管用
-        days_sensorData = []
+        mo = str(_yesterday.month)
+        # mo = "1"
 
-        # 一日ごとのセンサーデータを一月ごとに保管用
+        today = str(_yesterday.day)
+        #today = "1"
+
+        # センサーデータを一月ごとに保管用
         month_sensorData = {}
 
         # 今日の日付と昨日の日付を比較
         # 年は同じ
-        yesterdayPttn = self.isYesterday(self)
+        yesterdayPttn = self._isYesterday(self)
 
         # 昨日は昨年
         if (yesterdayPttn == 'lastYear'):
@@ -124,86 +135,80 @@ class GrafGenerater:
 
         # 昨日は月末
         elif (yesterdayPttn == 'lastMonth'):
-            # 昨日のセンサーデータリストを月Mapに詰め込み
-            month_sensorData.setdefault(str(_yesterday.month),jsonobj[str(_yesterday.month)])
+            # 昨日の月分のセンサーデータリストを月Mapに詰め込み
+            month_sensorData.setdefault(mo, {})
+            month_sensorData[mo] = jsonobj[mo]
 
-        # 今日の昨日は一日前
+        # 昨日
         elif (yesterdayPttn == 'yesterday'):
-
-            #昨日の表を作成
-            # logging.debug(len(jsonobj[str(_yesterday.month)]))
-            # logging.debug(type(jsonobj[str(_yesterday.month)]))
-
-            for sensordata in jsonobj[str(_yesterday.month)]:
-                # logging.debug(sensordata['dd'])
-                # 昨日の日と一致する日付のセンサーデータをリストに詰め込み
-                if (sensordata['dd'] == str(_yesterday.day)):
-                    # logging.debug(sensordata['dd'])
-                    days_sensorData.append(sensordata)
+            #logging.debug(jsonobj[mo][today])
+            #logging.debug('---')
 
             # 昨日のセンサーデータリストを月Mapに詰め込み
-            month_sensorData.setdefault(str(_yesterday.month),days_sensorData)
+            month_sensorData.setdefault(mo, {}).setdefault(today,{})
+            month_sensorData[mo][today] = jsonobj[mo][today]
 
         logging.debug('AA')
 
         return month_sensorData
 
-    def genGraphDataList(self, sensorData):
-        '''[summary]
+    def genGraf(self, sensorDataList):
+        '''[受け取ったデータでグラフを作成する．]
         
         Args:
-            sensordata ([type]): [description]
-        
-        Returns:
-            [type]: [description]
-        '''
+            sensorDataList ([dict]): [昨日，先月，去年のセンサーデータリスト]
+        '''        
 
         logging.debug('VV')
 
-        #graphList:ListにListを内包したものをグラフ描画のデータとする
-        #graphList = [[-10, 0, 10, 20, 30,40], [40, 30, 20, 10, 0,-10], [-10, 0, 0, 0,0, 40], [1, 2, 3, 4, 5,6], [-10, 40, 0, 0, 0,0]]
-        graphList = []
+        # sensorDataListからCO2の値を月別日別で取り出し，sensorCO2Listへ入れる．
+        monthList = sensorDataList.keys()
+        dayList = []
+        sensorCO2List=[]
+        #logging.debug('---')
 
-        # センサーデータを日にちごとに取りだす日にちの違い保管用
-        strDay = ''
+        for mo in monthList:
+            dayList.append(sensorDataList[mo].keys())
+            for day in sensorDataList[mo].keys():
+                sensorCO2List.append([int(_) for _ in sensorDataList[mo][day]['CO2']])
+        logging.debug(sensorCO2List)
+        #logging.debug('---')
         
-        #一日分のセンターデータ保管
-        dailyDataList = []
-
-        for key in sensorData.keys():
-            for dailyData in sensorData[key]:
-
-                sensorData_CO2= int(dailyData['CO2'])
-
-                if(dailyData['dd'] == strDay):
-                    dailyDataList.append(sensorData_CO2)
-
-                elif (len(dailyDataList) == 0):
-                    strDay = dailyData['dd']
-                    dailyDataList.append(sensorData_CO2)
-
-                else:
-                    graphList.append(dailyDataList)
-                    dailyDataList = []
-                    strDay = dailyData['dd']
-                    dailyDataList.append(sensorData_CO2)
-
-        # リストの最終末分を入れる．
-        if (len(dailyDataList) > 0):
-            graphList.append(dailyDataList)
-
-        logging.debug('AA')
-
-        return graphList
-
-    def genGraf(self,sensorDataList):
-        '''[summary]
+        #表をファイルに保存
+        saveFileName = 'graph.png'
         
-        Args:
-            sensorDataList ([type]): [description]
-        '''
+        # グラフ中のタイトル
+        graphTitle = ''
 
-        logging.debug('VV')
+        # 今日の日付と昨日の日付を比較
+        # 年は同じ
+        yesterdayPttn = self._isYesterday(self)
+
+        tmp_year =_yesterday.year
+        tmp_mo = _yesterday.month
+        tmp_today = _yesterday.day
+
+        # 表の表示サイズを固定 figsize=(width, height)
+        fig, ax = plt.subplots(figsize=(8,6))
+
+        # グラフのタイトルと，保存ファイル名に年月日をつける
+        # 昨日は昨年
+        if (yesterdayPttn == 'lastYear'):
+            saveFileName = '{}'.format(tmp_year)
+            graphTitle = '{} CO2濃度　PPM'.format(tmp_year)
+                        
+            # 表の表示サイズを固定 figsize=(width, height)
+            fig, ax = plt.subplots(figsize=(8, 30))
+
+        # 昨日は月末
+        elif (yesterdayPttn == 'lastMonth'):
+            saveFileName = 'Monthly'
+            graphTitle = '{}/{MM:02} CO2濃度　PPM'.format(tmp_year, MM=tmp_mo)
+
+        # 今日の昨日は一日前
+        elif (yesterdayPttn == 'yesterday'):
+            saveFileName = 'Daily'
+            graphTitle = '{}/{MM:02}/{DD:02} CO2濃度　PPM'.format(tmp_year, MM=tmp_mo, DD=tmp_today)
 
         # https://qiita.com/yniji/items/3fac25c2ffa316990d0c
         # 日本語を利用する場合のFont指定 <個別に>
@@ -213,14 +218,6 @@ class GrafGenerater:
         # 日本語を利用する場合のFont指定 <全体>
         rcParams['font.family'] = 'sans-serif'
         rcParams['font.sans-serif'] = ['IPAPGothic', 'VL PGothic']
-
-        # 表の表示サイズを固定
-        plt.figure(figsize=(12, 5))
-
-        #https://matplotlib.org/3.1.1/tutorials/colors/colormap-manipulation.html#sphx-glr-tutorials-colors-colormap-manipulation-py
-        # 描画領域の確保だけ、グラフは何も描画されない
-        #111の意味は、1行目1列の1番目という意味で、subplot(1,1,1)でも同じ.
-        plt.subplot(111)
 
         # 図の諸々設定
         #  気温
@@ -233,48 +230,33 @@ class GrafGenerater:
         # plt.imshow(rbm, cmap='RdBu_r',extent=(0, 48, 0, 1),aspect=1, vmin=0, vmax=100)
 
         # CO2
-        #plt.imshow(rbm, cmap='RdBu_r',extent=(0, 48, 0, 1),aspect=1, vmin=300, vmax=2000)
-        #plt.imshow(graphList, cmap='RdBu_r', extent=(0, 48, 0, 1), aspect=1)
-        plt.imshow(sensorDataList, cmap='RdBu_r', extent=(0, 48, len(sensorDataList), 0), aspect=1,vmin=300, vmax=1600)
+        im = ax.imshow(sensorCO2List, cmap='RdBu_r', extent=(0, 48, len(sensorCO2List), 0),  vmin=300, vmax=1600)
 
-        plt.title('CO2濃度　PPM')
-        plt.xlabel('X軸ラベル 時間')
+        ax.set_title(graphTitle)
+        ax.set_xlabel('時間 (一時間データをに2回取得なので48回　要調整)')
         # ax.set_xticklabels(farmers)
         # ax.set_yticklabels(vegetables)
-        plt.ylabel('Y軸ラベル 年月日')
+        ax.set_ylabel('日')
         #plt.legend('凡例')
 
         # X軸の開始～終わり
-        plt.xlim(0,48)
-        
+        ax.set_xlim(0, 48)
+        #ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(2))
+
         # Y軸の開始～終わり
-        #plt.ylim(1, 0)
-        plt.ylim(len(sensorDataList), 0)
+        ax.set_ylim(0,len(sensorCO2List))
 
-        #カラーバー
-        plt.colorbar()
-
-        #表をファイルに保存
-        # 2019/12/31-CO2 濃度.png _jsonfile = './{}_SensorData.json'.format(_yesterday.year)
-        saveFileName = 'graph.png'
+        #カラーバー colorbarの調整方法を調べてたらいつのまにか2時間経ってた 難しすぎでしょ 
+        divider = make_axes_locatable(ax)
+        # cax = divider.append_axes(position = 'right',size='2%', pad=0.1)
+        cax = divider.append_axes(position = 'right',size='2%', pad='2%')
+        plt.colorbar(im, cax=cax,orientation='vertical')
+        # plt.colorbar(im)
         
-        # 今日の日付と昨日の日付を比較
-        # 年は同じ
-        yesterdayPttn = self.isYesterday(self)
-        
-        # 昨日は昨年
-        if (yesterdayPttn == 'lastYear'):
-            saveFileName = '{}'.format(_yesterday.year)
+        #グリッド
+        ax.grid()
 
-        # 昨日は月末
-        elif (yesterdayPttn == 'lastMonth'):
-            saveFileName = '{}-{}'.format(_yesterday.year,_yesterday.month)
-
-        # 今日の昨日は一日前
-        elif (yesterdayPttn == 'yesterday'):
-            saveFileName = '{}-{}-{}'.format(_yesterday.year,_yesterday.month,_yesterday.day)
-
-        plt.savefig('D://etc//Desktop//'+ saveFileName + '-CO2濃度.png')
+        fig.savefig('D://etc//Desktop//'+ saveFileName + '-CO2濃度.png')
         #plt.show()
 
         logging.debug('AA')
@@ -283,9 +265,9 @@ class GrafGenerater:
         '''[summary]
         '''
 
-        repacked_sensorData = self.repackSensordata(self)
-        sensorDataList_CO2=self.genGraphDataList(self,repacked_sensorData)
-        self.genGraf(self,sensorDataList_CO2)
+        sensordata = self.repackSensordata(self)
+        #logging.debug(sensordata)
+        self.genGraf(self,sensordata)
 
     def __init__(self):
         ''''''
