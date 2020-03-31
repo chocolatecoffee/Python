3c95fac53f8cfaa89bd3a8a14fb7c7c3b774cbb8
0140831 <matsu.hide009@gmail.com>
2020-04-01 01:32:47 +0900

    特殊メソッド修正

--- Thread/ThreadOrder.py
+++ Thread/ThreadOrder.py
@@ -15,7 +15,7 @@ class ThreadOrder:
         with confu.ThreadPoolExecutor(max_workers=3, thread_name_prefix="thread") as executor:
             # executor.map(self.boil_udon, range(10))
             for order in orders:
-                executor.submit(self.Order, self, order)
+                executor.submit(self.Order, order)
 
         executor.shutdown
 
@@ -28,15 +28,35 @@ class ThreadOrder:
         orders = {'わかめ', 'コロッケ', 'カレー', '山菜',
                   'おあげ', '天ぷら', '牛すき', '鴨', 'そば', '具なし'}
 
-        self.RequestOrders(self, orders)
+        self.RequestOrders(orders)
+
+    def __new__(cls):
+        '''[summary]
+
+        Returns:
+            [type]: [description]
+        '''
+
+        logging.debug('__new__')
+        self = super().__new__(cls)
+        return self
 
     def __init__(self):
-        ''''''
+        '''[summary]
+        '''
+
+        logging.debug('__init__')
+
+    def __del__(self):
+        '''[summary]
+        '''
+        logging.debug('__del__')
 
 
 if __name__ == '__main__':
 
     logging.debug('VV')
-    myclass = ThreadOrder
-    myclass.Main(myclass)
+    myclass = ThreadOrder()
+    myclass.Main()
+    del myclass
     logging.debug('AA')
