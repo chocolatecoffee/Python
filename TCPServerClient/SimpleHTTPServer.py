fe1f221b07a37158844eee6ba0d9b51fd7256d26
0140831 <matsu.hide009@gmail.com>
2020-04-05 21:13:57 +0900

    破棄

--- TCPServerClient/SimpleHTTPServer.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-_PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", _PORT), Handler) as httpd:
-    print('Waiting...')
-    httpd.serve_forever()
