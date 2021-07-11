096502d6e702f0342149c7c6a381244826c770fc
chocolatecoffee <matsu.hide009@gmail.com>
2021-07-11 18:32:14 +0900

    気温を測定後（t_fineに保存）に気圧，または湿度を補正しないと値がおかしいままになる．これを修正．

--- twitter_bot/SensorBME280.py
+++ twitter_bot/SensorBME280.py
@@ -5,7 +5,6 @@
 # https://github.com/SWITCHSCIENCE/samplecodes/tree/master/BME280
 
 import smbus2 as SMBus
-import time
 import logging
 
 class bme280:
@@ -25,7 +24,6 @@ class bme280:
     logging.basicConfig(level=logging.DEBUG, filename='./Log.txt', filemode='w', format=' %(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
 
     def writeReg(self, reg_address, data):
-        
         self.bus.write_byte_data(self.i2c_address, reg_address, data)
 
     def get_calib_param(self):
@@ -72,53 +70,41 @@ class bme280:
                 self.digH[i] = (-self.digH[i] ^ 0xFFFF) + 1
 
     def compensate_P(self, pres_raw):
-        
-        # global t_fine
+
         pressure = 0.0
 
         v1 = (self.t_fine / 2.0) - 64000.0
         v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self.digP[5]
         v2 = v2 + ((v1 * self.digP[4]) * 2.0)
         v2 = (v2 / 4.0) + (self.digP[3] * 65536.0)
-        v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8) +
-              ((self.digP[1] * v1) / 2.0)) / 262144
+        v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self.digP[1] * v1) / 2.0)) / 262144
         v1 = ((32768 + v1) * self.digP[0]) / 32768
-
+        
         if v1 == 0:
             return 0
         pressure = ((1048576 - pres_raw) - (v2 / 4096)) * 3125
-        
         if pressure < 0x80000000:
             pressure = (pressure * 2.0) / v1
         else:
             pressure = (pressure / v1) * 2
-
-        v1 = (self.digP[8] * (((pressure / 8.0) *
-                               (pressure / 8.0)) / 8192.0)) / 4096
+        v1 = (self.digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
         v2 = ((pressure / 4.0) * self.digP[7]) / 8192.0
         pressure = pressure + ((v1 + v2 + self.digP[6]) / 16.0)
 
-        # print("pressure : %7.2f hPa" % (pressure / 100))
+        # print("pressure :{} hPa".format(pressure/100))
         
         return (pressure / 100)
 
     def compensate_T(self, temp_raw):
-        # global t_fine
         
-        v1 = (temp_raw / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
-        v2 = (temp_raw / 131072.0 - self.digT[0] / 8192.0) * \
+        self.t_fine = (temp_raw / 16384.0 - self.digT[0] / 1024.0) * self.digT[1] + (temp_raw / 131072.0 - self.digT[0] / 8192.0) * \
             (temp_raw / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
         
-        self.t_fine = v1 + v2
-        
-        # print("temp : %-6.2f ℃" % (temperature))
-
         return self.t_fine / 5120.0
 
     def compensate_H(self, hum_raw):
-        # global t_fine
-        var_h = self.t_fine - 76800.0
-        
+        var_h = self.t_fine- 76800.0
+
         if var_h != 0:
             var_h = (hum_raw - (self.digH[3] * 64.0 + self.digH[4]/16384.0 * var_h)) * (self.digH[1] / 65536.0 * (
                 1.0 + self.digH[5] / 67108864.0 * var_h * (1.0 + self.digH[2] / 67108864.0 * var_h)))
@@ -132,8 +118,6 @@ class bme280:
         elif var_h < 0.0:
             var_h = 0.0
 
-        # print("hum : %6.2f ％" % (var_h))
-        
         return var_h
 
     def setup(self):
@@ -162,12 +146,12 @@ class bme280:
 
         pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
         temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
-        hum_raw = (data[6] << 8) | data[7]
+        hum_raw  = (data[6] << 8)  |  data[7]
 
+        temp = self.compensate_T(temp_raw)
         pressure = self.compensate_P(pres_raw)
         hum = self.compensate_H(hum_raw)
-        temp = self.compensate_T(temp_raw)
-        
+
         return round(pressure, 2), round(hum, 2), round(temp, 2)
 
     def Main(self):
